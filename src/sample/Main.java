0f32d3c1e6c09d1d2c3860256f6646584f4d54ad
Cameron Nikiel <crn2357@g.rit.edu>
2019-03-19 16:27:12 -0400

    testing output

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello World, its works");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        System.out.println("I am test");
     }
 
 
